ceb40f17e23a951846eee98489c86f24a87590e0
niklasskoldmark <niklas.skoldmark@gmail.com>
2015-07-25 17:24:54 +0200

    enabled tail

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ echo "[smtp.gmail.com]:587 $GMAILADDRESS:$GMAILPASSWORD" >> /etc/postfix/sasl_pa
 postmap /etc/postfix/sasl_passwd
 #/etc/init.d/postfix start
 service postfix start
-#tail -F /var/log/mail.log
+tail -F /var/log/mail.log
